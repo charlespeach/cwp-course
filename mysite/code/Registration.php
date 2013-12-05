de9a665981ec39ba7dae4e71627d11c9ef082591
Charles Peach <charles@3months.com>
2013-12-05 16:30:38 +1300

    Add Custom hook before writing a registration to splits out the first name and the last name

--- mysite/code/Registration.php
+++ mysite/code/Registration.php
@@ -16,5 +16,29 @@ class Registration extends DataObject {
   private static $summary_fields = array(
     'Name','Organisation','Email','Status'
   );
-}
 
+  public function onBeforeWrite() {
+    if($this->Status == "Confirmed" && !Member::get()->filter('Email', $this->Email)->count()) {
+      $member = Member::create();
+      $member->FirstName = $this->FirstName;
+      $member->Surname = $this->Surname;
+      $member->Email = $this->Email;
+      $member->write();
+    }
+
+    parent::onBeforeWrite();
+  }
+
+  public function getFirstName() {
+    return $this->splitName(0);
+  }
+
+  public function getSurname() {
+    return $this->splitName(1);
+  }
+
+  private function splitName($id) {
+    $result = explode(' ', $this->Name);
+    return $result[$id];
+  }
+}
